a0ee82fc2ca4a9a015127ca376ed166c0f639eaa
LWilliams1568 <lashonwilliams9193@yahoo.com>
2021-01-26 17:08:43 -0500

    changed scaling for images

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBox = (props) => {
 	return (
-		<div className='col col-sm-4z'>
+		<div className='col col-sm-4'>
 			<input
 				className='form-control'
 				value={props.value}
