e0c59024bbeeee7170d8b1976ac8e97afd045a7e
LWilliams1568 <lashonwilliams9193@yahoo.com>
2021-01-26 13:33:20 -0500

    reverted changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddFavorites';
 import RemoveFavourites from './components/RemoveFavourites';
-import Logo from './components/logo';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
@@ -73,16 +72,10 @@ const App = () => {
 
 	return (
 		<div className='container-fluid'>
-		
-			<div className='row d-flex align-items-center mt-4 mb-4 movie-app fixMargin'>
-				
-				<Logo/>
-				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-			</div>
 			<div className='row d-flex align-items-center mt-4 mb-4 movie-app fixMargin'>
         
-				<MovieListHeading heading='Movies and TV Shows' />
-			
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
 			<div className='row movie-app sizing'>
 				<MovieList
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import '../App.css';
-
 
 const SearchBox = (props) => {
 	return (
-		<div className='col col-4'>
-		
-		<br></br>
+		<div className='col col-sm-4z'>
 			<input
-				className='form-control searchBar' 
+				className='form-control'
 				value={props.value}
 				onChange={(event) => props.setSearchValue(event.target.value)}
 				placeholder='Type to search...'
 			></input>
-		
 		</div>
 	);
 };
